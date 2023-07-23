cc982dbb8f324820c2f546c0fb0da3a3a036d39b
Venherova <139897339+Venherova@users.noreply.github.com>
2023-07-23 20:30:07 +0300

    Update app.js (hw2-4)

--- HW2/homework 2-4/app.js
+++ HW2/homework 2-4/app.js
@@ -2,5 +2,5 @@ let digit1 = Number(prompt('Enter first value'));
 let digit2 = Number(prompt('Enter second value'));
 let digit3 = Number(prompt('Enter third value'));
 
-alert((digit1 + digit2 + digit3)/3)
+alert((digit1 + digit2 + digit3)/3);
 
