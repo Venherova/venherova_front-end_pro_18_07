0c23297fdef15a47c5b614fc49f56015a60952e6
Elena Vengerova <elena@MacBook-Air-Elena.local>
2023-08-15 23:45:02 +0300

    Home work 12, Home work 13 changed functions

--- HW12/app.js
+++ HW12/app.js
@@ -1,10 +1,31 @@
-function removeElement(array, item) {
-  const index = array.indexOf(item);
+function removeElement(arr, item) {
+  const index = arr.indexOf(item);
   if (index > -1) {
-      array.splice(index, 1);
+      arr.splice(index, 1);
   }
 }
 
-const array = [1, 2, 3, 4, 5, 6, 7];
-removeElement(array, 5);
-console.log(array);
+function getValidNumberInput(message) {
+  let number;
+  do {
+      number = parseInt(prompt(message));
+  } while (isNaN(number));
+  return number;
+}
+
+let length = getValidNumberInput('Enter the length of the array:');
+
+let arr = [];
+
+for (let i = 0; i < length; i++) {
+    let value = getValidNumberInput(`Enter the ${i+1}-th element of the array:`);
+    arr.push(value);
+}
+
+console.log('Initial array:', arr);
+
+let itemToRemove = getValidNumberInput('Enter the item you want to delete:');
+
+removeElement(arr, itemToRemove);
+
+console.log('Array after deletion:', arr);
--- HW13/app.js
+++ HW13/app.js
@@ -1,10 +1,28 @@
 function pow(num, degree) {
-    if (degree === 0) return 1;
-    if (degree < 0) return 1 / pow(num, -degree);
+    if (degree === 0) {
+        return 1;
+    }
+
+    if (degree < 0) {
+        return 1 / pow(num, -degree);
+    }
+
     return num * pow(num, degree - 1);
 }
 
-let num = parseFloat(prompt('Enter the number you want to raise to a power:'));
-let degree = parseInt(prompt('Enter the power to which you want to raise the number:'));
+function getValidInput(message, type) {
+    let number;
+    do {
+        if (type === 'int') {
+            number = parseInt(prompt(message));
+        } else if (type === 'float') {
+            number = parseFloat(prompt(message));
+        }
+    } while (isNaN(number));
+    return number;
+}
+
+let num = getValidInput('Enter the number you want to raise to a power:', 'float');
+let degree = getValidInput('Enter the power to which you want to raise the number:', 'int');
 
 alert(`Result: ${pow(num, degree)}`);
